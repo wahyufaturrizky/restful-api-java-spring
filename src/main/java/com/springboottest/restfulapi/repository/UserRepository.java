8acf4ba802fa495df4e7321cc05dbe73db75984b
wahyufaturrizki <wahyufaturrizkyy@gmail.com>
2023-12-29 19:48:31 +0700

    FEAT: user argument resolver

--- src/main/java/com/springboottest/restfulapi/repository/UserRepository.java
+++ src/main/java/com/springboottest/restfulapi/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.springboottest.restfulapi.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.springboottest.restfulapi.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
+
+  Optional<User> findFirstByToken(String token);
   
 }
---
+++ src/main/java/com/springboottest/restfulapi/resolver/UserArgumentResolver.java
@@ -0,0 +1,44 @@
+package com.springboottest.restfulapi.resolver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpStatus;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.springboottest.restfulapi.entity.User;
+import com.springboottest.restfulapi.repository.UserRepository;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Component
+public class UserArgumentResolver implements HandlerMethodArgumentResolver {
+
+  @Autowired
+  UserRepository userRepository;
+
+  @Override
+  public boolean supportsParameter(MethodParameter parameter) {
+    return User.class.equals(parameter.getParameterType());
+  }
+
+  @Override
+  @Nullable
+  public Object resolveArgument(MethodParameter parameter, @Nullable ModelAndViewContainer mavContainer,
+      NativeWebRequest webRequest, @Nullable WebDataBinderFactory binderFactory) throws Exception {
+        HttpServletRequest serveletRequest = (HttpServletRequest) webRequest.getNativeRequest();
+        String token = serveletRequest.getHeader("X-API-TOKEN");
+
+        if (token == null) {
+          throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Unauthorized");
+        }
+
+        return userRepository.findFirstByToken(token).orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Unauthorized"));
+  }
+  
+}
